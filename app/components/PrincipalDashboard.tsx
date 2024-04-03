8cbb71b0d12c19d7c91eb116d00fb10d09927631
KenyOnFireBAK <geronimoplay@gmail.com>
2024-04-03 16:13:52 +0200

    New File & divs with flex

---
+++ app/components/PrincipalDashboard.tsx
@@ -0,0 +1,14 @@
+const PrincipalDashboard = () => {
+    return (
+        <>
+            <div className="flex flex-row space-x-4">
+                <div className="h-50 w-20 bg-cyan-500">1</div>
+                <div className="h-50 w-20 bg-cyan-600">2</div>
+                <div className="h-50 w-20 bg-cyan-700">3</div>
+                <div className="h-50 w-20 bg-cyan-800">4</div>
+            </div>
+        </>
+    )
+}
+
+export default PrincipalDashboard
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
 import NavBar from '../components/NavBar';
+import PrincipalDashboard from '../components/PrincipalDashboard';
 export const meta: MetaFunction = () => {
   return [
     { title: "New Remix App" },
@@ -12,9 +13,9 @@ export default function Index() {
     <>
       <div className="grid grid-cols-[auto,1fr] gap-[20px] h-screen p-8">
         <nav className="h-full border bg-white space-y-8 shadow-lg rounded-md"><NavBar/></nav>
-        <div className="bg-gray-300 rounded-md overflow-y-auto">Content Container</div>
+        <div className="bg-gray-300 rounded-md overflow-y-auto"><PrincipalDashboard/></div>
       </div>
     </>
-  );
+  )
 }
 
