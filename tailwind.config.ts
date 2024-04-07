bb296e2c7e75c25ec7275fa8ba2c6bd3656e9ecb
FunSociety <holagabrift@gmail.com>
2024-04-07 18:37:09 +0200

    Try to fix vercel bug with flowbite

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,15 @@
+/* eslint-disable import/no-named-as-default-member */
 import type { Config } from "tailwindcss";
-import {content, plugin} from 'flowbite-react/tailwind'
+// import {content, plugin} from 'flowbite-react/tailwind'
+import flowbite from "flowbite-react/tailwind";
 export default {
   content: [
     "./app/**/*.{js,jsx,ts,tsx}",
     "./node_modules/flowbite-react/**/*.js",
-    content()
+    flowbite.content()
   ],
   theme: {
     extend: {},
   },
-  plugins: [require("flowbite/plugin"), plugin()],
+  plugins: [flowbite.plugin()],
 } satisfies Config;
