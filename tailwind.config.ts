102f97f6e1c5a8bbc11b2de59089bd037a02d3e5
FunSociety <holagabrift@gmail.com>
2024-04-07 18:28:20 +0200

    Implement Flowbite Framework & test basic component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,13 @@
 import type { Config } from "tailwindcss";
-
+import {content, plugin} from 'flowbite-react/tailwind'
 export default {
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/flowbite-react/**/*.js",
+    content()
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin"), plugin()],
 } satisfies Config;
