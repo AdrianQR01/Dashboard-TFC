5eabe494ce9ccbfbba6a8afd40ed72311c29c72e
AdrianQR <106702080+AdrianQR01@users.noreply.github.com>
2024-04-22 20:25:54 +0200

    Fix Nabvar arriba
    Tenias puesto dentro del header el nabvar por eso te salia arriba

--- app/root.tsx
+++ app/root.tsx
@@ -24,13 +24,12 @@ export function Layout ({ children }: { children: React.ReactNode }) {
       </head>
       <body>
         <div className="grid grid-rows-[1fr,auto] gap-[20px] w-screen p-8 sm:grid-cols-[auto,1fr] h-screen">
-          <header>
-            <nav className="h-full border bg-white space-y-8 shadow-lg rounded-md row-span-3 order-last sm:order-first"><NavBar/></nav>
-          </header>
           {children}
+          <nav className="h-full border bg-white space-y-8 shadow-lg rounded-md row-span-3 order-last sm:order-first"><NavBar/></nav>
         </div>
         <ScrollRestoration />
         <Scripts />
+        
       </body>
     </html>
   )
