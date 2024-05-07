921ea3b7ac43dfa4da4f412d3d3399ed85b1f79a
FunSociety <holagabrift@gmail.com>
2024-05-07 10:35:01 +0200

    Add new charts, and make prisma orm config

--- app/routes/dashboard._index.tsx
+++ app/routes/dashboard._index.tsx
@@ -2,6 +2,11 @@ import type { MetaFunction } from '@remix-run/node'
 import AreaChart from '~/components/areachart'
 import ColumnChart from '~/components/columnchart'
 import LineChart from '~/components/linechart'
+import PieChart from '~/components/piechart'
+import DonutChart from '~/components/donutchart'
+import RadialChart from '~/components/radialchart'
+import { json } from "@remix-run/node"
+import { useLoaderData } from "@remix-run/react"
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,12 +14,25 @@ export const meta: MetaFunction = () => {
     { name: 'description', content: 'This is the content for dashboard index' }
   ]
 }
+
+export async function loader(){
+    return json({
+        "dato1": "tessst",
+        "dato2": "https://randomuser.me/api/portraits/women/79.jpg",
+    });
+};
+
 export default function DashboardIndex () {
   const tags = [
     <LineChart key='line'/>,
     <AreaChart key='area'/>,
-    <ColumnChart key='column'/>
+    <ColumnChart key='column'/>,
+    <PieChart key ='pie'/>,
+    <DonutChart key='donut'/>,
+    <RadialChart key='radial'/>
   ]
+  const userData = useLoaderData<typeof loader>();
+  console.log(userData)
   return (
     <div className="flex flex-wrap items-center justify-center">
         {tags.map((tag, index) => (
--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,14 +1,28 @@
 import { Outlet } from '@remix-run/react'
 import Navbar from '../components/navbar'
+import { json } from "@remix-run/node"
+import { useLoaderData } from "@remix-run/react"
 
-export default function Dashboard () {
+export async function loader(){
+    return json({
+        "name": "Alivika Tony",
+        "avatar": "https://randomuser.me/api/portraits/women/79.jpg",
+    });
+};
+
+export default function Dashboard() {
+  const userData = useLoaderData<typeof loader>();
+  console.log(userData)
   return (
     <div className="rounded-md overflow-y-auto row-spam">
-        <div className="grid grid-rows-[1fr,auto] gap-[20px] w-screen p-8 sm:grid-cols-[auto,1fr] h-screen">
-            <nav className="h-full border bg-white space-y-8 shadow-lg rounded-md row-span-3 order-last sm:order-first">
-                <Navbar/>
-            </nav>
-            <Outlet/>
+        <div className="grid grid-rows-[1fr,auto] gap-x-[20px] p-8 sm:grid-cols-[auto,1fr] max-h-screen">
+            <nav className="border bg-white space-y-8 shadow-lg rounded-md row-span-3 order-last sm:order-first">
+              <Navbar/>
+            </nav> 
+            <main className='overflow-y-auto'>
+              <Outlet/>
+            </main>
+
         </div>
     </div>
   )
