6a1da3e9744d3bb74ccca17c6b5a075f5aaee009
FunSociety <holagabrift@gmail.com>
2024-05-16 10:36:33 +0200

    Update dashboard.entradas.tsx

--- app/routes/dashboard.entradas.tsx
+++ app/routes/dashboard.entradas.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from '@remix-run/react'
+import RadialChart from '~/components/radialchart'
 
 export default function Entradas() {
     return (
         <div>
+            <RadialChart/>
             <Outlet/>
             Estas en Entradas
         </div>
