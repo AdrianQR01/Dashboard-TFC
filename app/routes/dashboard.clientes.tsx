dce766b8f3654a7b1dc5631db196d34f4d1d30b2
AdrianQR <106702080+AdrianQR01@users.noreply.github.com>
2024-05-16 09:10:00 +0200

    Add Table

---
+++ app/routes/dashboard.clientes.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from '@remix-run/react'
+import { Tabla } from '~/components/tabla'
+
+
+export default function Clientes() {
+    return (
+        <div>
+            <Outlet/>
+            <Tabla />
+        </div>
+    )
+}
