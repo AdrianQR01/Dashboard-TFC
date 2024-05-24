4a9baa91fa05b9c81a8a08caa9cdf308beabedd6
FunSociety <holagabrift@gmail.com>
2024-05-24 12:24:32 +0200

    Update dashboard.presupuestos.tsx

--- app/routes/dashboard.presupuestos.tsx
+++ app/routes/dashboard.presupuestos.tsx
@@ -10,17 +10,17 @@ export default function Presupuestos() {
     <div className="container mx-auto px-4">
       <h1 className="text-2xl font-bold mt-4">EventCo Summit 2023</h1>
       <Tabs aria-label="Tabs with underline" style="underline">
-      <Tabs.Item title="Gastos">
-        This is <span className="font-medium text-gray-800 dark:text-white">Dashboard tab's associated content</span>.
-        Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
-        control the content visibility and styling.
-      </Tabs.Item>
-      <Tabs.Item title="Ingresos">
-        This is <span className="font-medium text-gray-800 dark:text-white">Settings tab's associated content</span>.
-        Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
-        control the content visibility and styling.
-      </Tabs.Item>
-    </Tabs>
+        <Tabs.Item title="Gastos">
+          This is <span className="font-medium text-gray-800 dark:text-white">Dashboard tab's associated content</span>.
+          Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
+          control the content visibility and styling.
+        </Tabs.Item>
+        <Tabs.Item title="Ingresos">
+          This is <span className="font-medium text-gray-800 dark:text-white">Settings tab's associated content</span>.
+          Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
+          control the content visibility and styling.
+        </Tabs.Item>
+      </Tabs>
       <div className="mt-6">
         <h2 className="text-xl font-semibold">Revenue</h2>
         <div className="bg-white shadow-md rounded-lg p-6 mt-4">
