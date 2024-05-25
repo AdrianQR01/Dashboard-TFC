e3480b7311281cd0df31bbe16ce0b587132d5af9
AdrianQR <106702080+AdrianQR01@users.noreply.github.com>
2024-05-25 12:43:24 +0200

    Create dashboard.servicios.tsx

---
+++ app/routes/dashboard.servicios.tsx
@@ -0,0 +1,10 @@
+import { Outlet } from '@remix-run/react'
+
+export default function Servicios() {
+    return (
+        <div>
+            <Outlet/>
+            Estas en Servicios
+        </div>
+    )
+}
