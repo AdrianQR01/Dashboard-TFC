b8f189c7bec9574a636bc870c3b81175b439855f
FunSociety <holagabrift@gmail.com>
2024-06-05 14:50:23 +0200

    Table responsive process

--- app/routes/dashboard.presupuestos._index.tsx
+++ app/routes/dashboard.presupuestos._index.tsx
@@ -4,19 +4,21 @@ import { useLoaderData } from "@remix-run/react";
 import TablaTest from "./components/TablaTest";
 
 export async function loader({ request }: LoaderFunctionArgs) {
-    const data = [
-      { id: 1, name: 'John', age: 25 },
-      { id: 2, name: 'Jane', age: 30 },
-      { id: 3, name: 'Bob', age: 35 },
-    ];
-  
-    return json(data);
-  }
+  const data = [
+    { id: 1, name: 'John', age: 25 },
+    { id: 2, name: 'Jane', age: 30 },
+    { id: 3, name: 'Bob', age: 35 },
+  ];
+
+  return json(data);
+}
 export default function General() {
-    const data = useLoaderData<typeof loader>();
-    return (
-        <div className="flex flex-wrap">
-            <div className="w-full h-full"><TablaTest data={data} /></div>
-        </div>
-    )
+  const data = useLoaderData<typeof loader>();
+  return (
+    <div className="flex flex-wrap">
+      <div className="w-full h-full">
+        <TablaTest data={data} />
+      </div>
+    </div>
+  )
 }
